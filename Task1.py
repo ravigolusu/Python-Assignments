fae1ad4f11a5ee0f779f1023ca16f3919fb89cb3
Golusu Ravi Teja <154101105+ravigolusu@users.noreply.github.com>
2025-09-03 23:11:16 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,7 @@
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+
+print('Addition: ', int(a) + int(b))
+print('Subtraction: ', int(a) - int(b))
+print('Multiplication: ', int(a) * int(b))
+print('Division: ', int(a) / int(b))
---
+++ Task2.py
@@ -0,0 +1,4 @@
+f = input("Enter the first name: ")
+s =  input("Enter the second name: ")
+
+print("Hello, " + (str(f)+" "+str(s)) + "!"+" "+"Welcome to the Python program")
